e617f99db2e93f740f0bd20fb07dff1ac23d983b
vlouggos <44415066+vlouggos@users.noreply.github.com>
2023-01-01 23:58:49 +0200

    1

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ import os
 app = Flask(__name__)
 DATABASE_URL = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+db = SQLAlchemy(app)
+
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return DATABASE_URL
+    new_book = Book(title="TITLE", author="AUTHOR", rating=10)
+    db.session.add(new_book)
+    return book.title
 
 
 
